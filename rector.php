29d0c1656416c7f44017987a82ed67c41a94c444
iz-ahmad <n.ahmad.web.cit22@gmail.com>
2026-04-06 14:33:59 +0600

    update rector.php to target upto PHP 8.4

--- rector.php
+++ rector.php
@@ -14,7 +14,7 @@ return static function (RectorConfig $rectorConfig): void {
 
     // Define what rule sets will be applied
     $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_81,
+        LevelSetList::UP_TO_PHP_84,
         SetList::CODE_QUALITY,
         SetList::DEAD_CODE,
         SetList::EARLY_RETURN,
